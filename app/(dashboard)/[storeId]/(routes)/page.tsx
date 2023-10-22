5567e71ccee5133c12d4a0d98b6d9c504dddd60b
Mallory Daniel <mllr.dnl@gmail.com>
2023-10-22 16:26:45 -0700

    Redirected to dashboard after store creation

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>This is a Dashboard!</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -13,11 +13,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      <p>Root Page</p>
-    </div>
-  );
+  return null;
 };
 
 export default SetupPage;
